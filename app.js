47bc8ae29a65291b5698d85276c5f38411581c2f
Josh <leong.jo@gmail.com>
2017-09-11 15:00:46 -0600

    working single lang translation

--- app.js
+++ app.js
@@ -19,23 +19,39 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
 var wInput = document.getElementById('w-input').value;
-var endpoint = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170908T053655Z.87f4c706c2741f07.fdc80dc56fa59728c399270343443eacb394ecfc&lang=en-fr&text=' + wInput;
+var endpointfr = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170908T053655Z.87f4c706c2741f07.fdc80dc56fa59728c399270343443eacb394ecfc&lang=en-fr&text=' + wInput;
 
 
 
-  fetch(endpoint)
+
+
+
+
+
+
+  fetch(endpointfr)
           .then((response) => {
             return response.json()
             .then((translateData) => {
-              var sectiontag = document.getElementById('tWord');
               var newH1 = document.createElement('h1');
               newH1.innerHTML = translateData.text["0"];
-
-console.log(translateData.text["0"]);
+              var test = document.getElementById('tWord')
+              test.append(newH1)
 
             })
           })
         })
+
+
+
+
+
+
+
+
+
+
+
 
 
 
